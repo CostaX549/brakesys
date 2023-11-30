90360ec75be1aed1c57d63c992301b74e3c1b8e5
CostaX549 <costasantos896@gmail.com>
2023-11-30 12:47:46 -0300

    Mudanças 30/11/2023 2.0

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -1,7 +1,12 @@
-
+//components
+import TestimonialSlider from "./TestimonialSlider"
 
 export default function Testimonial() {
   return (
-    <section className='section flex items-center bg-cyan-300' id='testimonial'>Testimonial</section>
+    <section className='section flex items-center' id='testimonial'>
+      <div className="container mx-auto">
+      <TestimonialSlider />
+      </div>
+    </section>
   )
 }
--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -1,7 +1,78 @@
+"use client"
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules'
+
+// icons
+import { FaQuoteLeft } from 'react-icons/fa';
+
+// framer motion
+import { motion } from 'framer-motion'
+
+// variants
+import { fadeIn } from '/variants'
+
+// next image
+import Image from 'next/image'
+
+// data
+const testimonialData = [
+  {
+    message: 'They truly exceeded my expectations and made my car rental experience a delight',
+    avatar: '/images/testimonial/avatar.png',
+    name: 'Jane Doe',
+    job: 'Photographer & Videographer'
+
+
+  },
+  {
+    message: 'They truly exceeded my expectations and made my car rental experience a delight',
+    avatar: '/images/testimonial/avatar.png',
+    name: 'Jane Doe',
+    job: 'Photographer & Videographer'
+
+
+  }
+]
+
 
 
 export default function TestimonialSlider() {
   return (
-    <div>TestimonialSlider</div>
+    <motion.div
+    variants={fadeIn('up', 0.4)}
+    initial="hidden"
+    whileInView={"show"}
+    viewport={{ once: false, amount: 0.6 }}
+    className='container mx-auto'
+    >
+      <Swiper pagination={{
+        clickable: true,
+        dynamicBullets: true,
+      }}
+      modules={[Pagination]}
+      className='h-[450px] xl:h-[400px]'
+      >
+        {testimonialData.map((person, index) => {
+          const {message, avatar, name, job} = person
+          return <SwiperSlide key={index}>
+            <div className='flex flex-col justify-center items-center text-center'>
+              <FaQuoteLeft className='text-7xl text-accent mb-6' />
+              <div className='text-2xl xl:text-4xl max-w-[874px] mb-12 font-medium'>{message}</div>
+              <Image src={avatar} width={64} height={64} alt='' className='mb-4' />
+              <div className='text-lg font-medium'>{name}</div>
+              <div className='text-secondary'>{job}</div>
+            </div>
+          </SwiperSlide>;
+        })}
+      </Swiper>
+    </motion.div>
   )
 }
