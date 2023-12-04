def3f582ac359f2e120877e5dd8fef9e0d95ff81
CostaX549 <costasantos896@gmail.com>
2023-12-03 22:08:19 -0300

    Mudanças Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { SearchContextProvider } from './context/search'
 import { Inter } from 'next/font/google'
-import Head from 'next/head';
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'BrakeSYS',
   description: 'brakesys',
+ verification: {
+  google: "nun9UckWe_3wFzC44_J99JvDc0AncCdeglBNzTb4uwg"
+ },
+ generator: 'Next.js',
+ applicationName: 'brakesys',
+ referrer: 'origin-when-cross-origin',
+ keywords: ['Next.js', 'Mecânica', 'BrakeSYS'],
+ authors: [{ name: 'Junior' }, { name: 'Senai', url: 'https://nextjs.org' }],
+ creator: 'Junior',
+ publisher: 'Junior',
+ formatDetection: {
+   email: false,
+   address: false,
+   telephone: false,
+ },
   
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+"use client"
 import BackToTopBtn from "./components/BackToTopBtn";
 import Cta from "./components/Cta";
 
 import Header from "./components/Header";
 import About from "./components/About";
-import Cars from "./components/Cars";
+
 import  Why from "./components/Why";
 import  Testimonial from "./components/Testimonial";
 
@@ -12,9 +13,14 @@ import Hero from "./components/Hero";
 
 
 
+
 export default function Home() {
+
   return (
+    
   <main className='max-w-[1920px] bg-white mx-auto relative'>
+ 
+
     <Header />
     <Hero />
 
